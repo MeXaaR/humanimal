f812b9f7dba9a8b7a13884538c5276d764f29895
Francois AUBEUT <francois.aubeut-gervaise@carrier.com>
2024-11-17 20:29:49 +0100

    add promise to search params

--- types/pages.d.ts
+++ types/pages.d.ts
@@ -1,4 +1,4 @@
 export type PageProps<T = { [key: string]: string }> = {
     params: Promise<T>;
-    searchParams: { [key: string]: string | string[] | undefined };
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
